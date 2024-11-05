fc669c3de94969e57d89f939fa98392d0311e44c
Teja Annamdevula <tejaannamdevula@gmail.com>
2024-11-05 23:54:38 +0530

    feat:learnt keyframes

--- 2024/FramerMotion/web/components/animations/KeyFrame.tsx
+++ 2024/FramerMotion/web/components/animations/KeyFrame.tsx
@@ -1,10 +1,26 @@
 "use client";
 import { motion } from "framer-motion";
 
+/* 
+
+    Keyframes 
+
+    splitting or dividing animation into mutiple parts
+    
+        [start,end]
+
+*/
 export function KeyFrame() {
   return (
     <div>
-      <motion.div className="w-[300px] h-[300px] bg-orange-600"></motion.div>
+      <motion.div
+        className="w-[300px] h-[300px] bg-orange-600"
+        animate={{
+          // scale: [1, 2, 4, 5, 2, 1, 2, 3],
+          rotate: [10, 20, 30, 40, 50],
+        }}
+        transition={{ duration: 5 }}
+      ></motion.div>
     </div>
   );
 }
